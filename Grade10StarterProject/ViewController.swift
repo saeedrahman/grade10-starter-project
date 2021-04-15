6bbd066870fedeb11e1bb5a6311f26a60a0e784b
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2021-04-15 12:24:44 +0800

    Initial Commit

---
+++ Grade10StarterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grade10StarterProject
+//
+//  Created by Saeed Rahman on 15/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
